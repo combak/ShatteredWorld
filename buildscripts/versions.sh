a551e8f2aa683c82a5f74dcff58270883dc2521d
DerOli82 <DerOli82@alaoli.de>
2017-08-24 17:17:51 +0200

    new structure

--- buildscripts/versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "## Forge" > Versions.md
-echo "-" $(unzip -l bin/modpack.jar | grep forge- | sed 's/^.*forge-/forge-/' | sed 's/-changelog.txt//') >> Versions.md
-echo "" >> Versions.md
-echo "## mods" >> Versions.md
-cd mods
-ls --format single-column *.jar | sed 's/^/- /' >> ../Versions.md
-cd ..
--- contentScripts/Items.zs
+++
@@ -1,2 +0,0 @@
-# LocalizedNameString, UnlocalizedNameString, TextureNameString, [CreativeTabString, MaxDamage, MaxStackSize, ToolTypeString, ToolLevel, isFull3D, LoreArrayString]
-mods.content.Item.registerItem( "Pile of wood logs", "pileOfWoodLogs", "pileOfWoodLogs" );
